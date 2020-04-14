f41944008234eaba76c126ee16aa5218bb57b812
fredlin <forest.redlin@gmail.com>
2020-04-14 11:25:14 -0700

    mysterious organism challenge now runs for all test cases. need to seek advice on mutate simplification and willLikelySurvive edge case testing function (currently always true)

--- mysterious-organism-codecademy.js
+++ mysterious-organism-codecademy.js
@@ -1,15 +1,21 @@
-// Meal Maker Project Codecademy
+// Mysterious Organinism Project Codecademy
 // Date: April 14, 2020
 // Author: fredlin
 
+
 // Returns a random DNA base
+// given in project setup
 const returnRandBase = () => {
   const dnaBases = ['A', 'T', 'C', 'G'];
   return dnaBases[Math.floor(Math.random() * 4)];
   console.log(dnaBases);
 };
+// currently, I am not using this function at all (although mockUpStrand calls it)
+// I suspect there is a way to use this function as a helper to the mutate method
+
 
 // Returns a random single stand of DNA containing 15 bases
+// given in project setup
 const mockUpStrand = () => {
   const newStrand = [];
   for (let i = 0; i < 15; i++) {
@@ -17,64 +23,53 @@ const mockUpStrand = () => {
   };
 
   return newStrand;
-  console.log(newStrand);
 };
-
-// I think I will use this when calling my factory function
-// Will need to picture out how to number each new strand
-//
 
 
 
 // Factory Function
-const pAequorFactory = (num, array) => {
-  const dnaObject = {
-    specimenNum: num, // a unique identifer for the strand of dna
-    dna: array, // a strand of dna of lengths 15
-    mutuate() {
+const pAequorFactory = (specNum, dnaStrand) => {
+  return {
+    specimenNum: specNum, // a unique identifer for the strand of dna
+    dna: dnaStrand, // a strand of dna of length 15
+    mutate() {
       // there must be a better way.....
-      const randomIndex = Math.floor(Math.random() * 15);
+      const randomIndex = Math.floor(Math.random() * this.dna.length);
       const dnaBase = this.dna[randomIndex];
-      if (dnaBase === 'A') { // could use switch here
-        let newDnaBase = ['T', 'C', 'G'];
-        let newRandomIndex = Math.floor(Math.random() * 3);
-
-        // randomly select an index in the dna array
-
-        this.dna[newRandomIndex] = newDnaBase;
-
-        // change that base to a random base of the 3 remmain types
+      if (dnaBase === 'A') { // could use switch here or adapt to all returnRand Base helper
+        let newDnaBaseArray = ['T', 'C', 'G'];
+        let newRandomIndex = Math.floor(Math.random() * 3);   // randomly select an index in the shortened dna array
+        this.dna[randomIndex] = newDnaBaseArray[newRandomIndex]; // change base in object strand to a random base of the 3 remaining types
 
       } else if (dnaBase === 'T') {
-        let newDnaBase = ['A', 'C', 'G'];
+        let newDnaBaseArray = ['A', 'C', 'G'];
         let newRandomIndex = Math.floor(Math.random() * 3);
-        this.dna[newRandomIndex] = newDnaBase;
+        this.dna[randomIndex] = newDnaBaseArray[newRandomIndex];
+
       } else if (dnaBase === 'C') {
-        let newDnaBase = ['A', 'T', 'G'];
+        let newDnaBaseArray = ['A', 'T', 'G'];
         let newRandomIndex = Math.floor(Math.random() * 3);
-        this.dna[newRandomIndex] = newDnaBase;
-      } else if (dnaBase === 'G') { // should this still be else if?
-        let newDnaBase = ['A', 'T', 'C'];
+        this.dna[randomIndex] = newDnaBaseArray[newRandomIndex];
+
+      } else if (dnaBase === 'G') {
+        let newDnaBaseArray = ['A', 'T', 'C'];
         let newRandomIndex = Math.floor(Math.random() * 3);
-        this.dna[newRandomIndex] = newDnaBase;
+        this.dna[randomIndex] = newDnaBaseArray[newRandomIndex];
       }
 
-      return this.dna; // right position?
+      return this.dna;
     },
 
     compareDNA(objectToCompare) { // this object contains a specimenNum and a dna array
-      const indexMatchCounter = 0;
+      let indexMatchCounter = 0;
       for (let i = 0; i < 15; i++) { // loop over input object
         if (objectToCompare.dna[i] === this.dna[i]) { // I am not confident that these pathways are accurate.
           indexMatchCounter++;
         };
       };
 
-      const percentCommon = indexMatchCounter / 15;
-      console.log(`Specimen ${objectToCompare.specimenNum} and specimen
-      ${this.dnaObject.specimenNum} have ${indexMatchCounter} DNA in common.`);
-
-      // I still do not understand why this is the correct syntax for logging js within objects/methods
+      const percentCommon = Math.floor((indexMatchCounter / 15) * 100);
+      console.log(`Specimen ${objectToCompare.specimenNum} and specimen ${this.specimenNum} have ${percentCommon}% DNA bases in common.`);
     },
 
     willLikelySurvive() {
@@ -95,37 +90,39 @@ const pAequorFactory = (num, array) => {
       };
     }
   };
-  return dnaObject;
 };
 
-
+// To store 30 sample strands in an array where willLikelySurvive == true
 let arrayOf30 = [];
+let specimenNum = 1; // initialize a unique specimenNum the will be applied to each object
 
 while (arrayOf30.length < 30) {
-  let specimenNum = 1;
-  let tinCan = pAequorFactory(specimenNum, returnRandBase);
+  let tinCan = pAequorFactory(specimenNum, mockUpStrand());
   if (tinCan.willLikelySurvive() === true) {
     arrayOf30.push(tinCan);
     specimenNum++;
   };
 };
 
-console.log(arrayOf30);
+// // Test Commands
 
+// // arrayOf30 test
+// console.log(arrayOf30);
 
-
-
-
-// create 30 instances using factory function that can survice
-// store in an array (what about indenifier?)
-// create a while loop to keep running factory function till the array has lenght = 30
-// create an if statement to push new array to list IF stay passes survival test
-// where does mutation come into this?
-
-// to Call function when needed
-// const tinCan = pAequorFactory(x, y);
+// // mutate method
+// const strand1 = pAequorFactory(111, mockUpStrand());
+// console.log(`strand1 before mutatation: ${strand1.dna}`);
+// strand1.mutate();
+// console.log(`strand1 after mutatation: ${strand1.dna}`);
 //
-// tinCan();
-
-// returnRandBase();
-// Can you call a function that is defined as a varibale?
+// // compareDNA method
+// const strand2 = pAequorFactory(222, mockUpStrand());
+// const strand3 = pAequorFactory(333, mockUpStrand());
+//
+// // strand2.compareDNA(strand3);
+//
+// // willLikelySurvive method
+// console.log(strand2);
+// const strand2SurvivalChances = strand2.willLikelySurvive();
+// console.log(`Chance of survival (true = high & false = low): ${strand2SurvivalChances}`);
+// // never seen this go false...
